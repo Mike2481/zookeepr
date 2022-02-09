393173fc3930df975687c304b42340926c9e125a
Mike Gordon <vprmatrix55@gmail.com>
2022-02-08 19:30:23 -0700

    add zookeepers

---
+++ __tests__/zookeepers.test.js
@@ -0,0 +1,83 @@
+const fs = require('fs');
+const { filterByQuery, findById, createNewZookeeper, validateZookeeper } = require('../lib/zookeepers');
+
+const { zookeepers } = require('../data/zookeepers.json');
+const { TestWatcher } = require('jest');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const res = require('express/lib/response');
+
+jest.mock('fs');
+
+test('Creates a zookeeper object', () => {
+    const zookeeper = createNewZookeeper (
+        {name: Mike, id: 8675309 },
+        zookeepers
+    );
+
+    expect(zookeeper.name).toBe('Mike');  
+    expect(zookeeper.id).toBe(8675309);
+});
+
+test('filters by query', () => {
+    const startingZookeepers = [
+        {
+          id: "2",
+          name: "Raksha",
+          age: 31,
+          favoriteAnimal: "penguin",
+        },
+        {
+          id: "3",
+          name: "Isabella",
+          age: 67,
+          favoriteAnimal: "bear",
+        },
+      ];
+
+      const updatedZookeepers = filterByQuery({ age: 31}, startingZookeepers);
+
+      expect(updatedZookeepers.length).toEqual(1);
+});
+
+test('finds by id', () => {
+    const startingZookeepers = [
+        {
+          id: "2",
+          name: "Raksha",
+          age: 31,
+          favoriteAnimal: "penguin",
+        },
+        {
+          id: "3",
+          name: "Isabella",
+          age: 67,
+          favoriteAnimal: "bear",
+        },
+      ];
+
+      const result = findById("3", startingZookeepers);
+
+      expect(result.name).toBe("Isabella");
+});
+
+test('validates Zookeeper Age is a number', () => {
+    const zookeeper = {
+        id: "2",
+        name: "Raksha",
+        age: 31,
+        favoriteAnimal: "penguin",
+      };
+    
+      const invalidZookeeper = {
+        id: "3",
+        name: "Isabella",
+        age: "67",
+        favoriteAnimal: "bear",
+      };
+    
+      const result = validateZookeeper(zookeeper);
+      const result2 = validateZookeeper(invalidZookeeper);
+    
+      expect(result).toBe(true);
+      expect(result2).toBe(false);
+});
